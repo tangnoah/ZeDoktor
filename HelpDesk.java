1e3ade075b0f39b27ff092e663089253c7a22aa7
HermanLin <hlin9@stuy.edu>
2017-04-21 10:10:22 -0400

    started HelpDesk

---
+++ HelpDesk.java
@@ -0,0 +1,18 @@
+/*Team ZeDoktor: Herman Lin, Jesse Sit, Noah Tang
+APCS2 - pd3
+LAB03 -- RUVIP?
+2017-4-21*/
+
+import java.util.Scanner;
+
+public class HelpDesk {
+
+    //INSTANCE VARIABLES
+    private String nombre;
+    private String problema;
+    private int importante;
+    
+    public static void main (String[] args) {
+	System.out.println("\nHeLlO aNd WeLcOmE tO tHe HeLp DeSk!\n\n");
+    }    
+}
