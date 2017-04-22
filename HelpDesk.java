9f852e8c0c6c32b954d35725081e5f5a287f516e
tangnoah <ntang@stuy.edu>
2017-04-22 19:58:16 -0400

    improved input

--- HelpDesk.java
+++ HelpDesk.java
@@ -13,22 +13,53 @@ public class HelpDesk {
     private static int importante;
     private static int[] uniqueIDs;
     
+    public static void clear(){
+	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+
+
     public static void main (String[] args) {
 	Scanner scanner = new Scanner(System.in);
-	int inputInt;
-	int inputID;
+	int inputInt = -1;
+	int inputID = -1;
 	String inputStr;
-	
-	System.out.println("\nHeLlO aNd WeLcOmE tO tHe HeLp DeSk!\n\n");
-	System.out.println("Please choose an option:\n");
-	System.out.print("1:\tI have a Ticket ID\n");
-	System.out.print("2:\tI have a problem\n");
-	System.out.print("3:\tProblem solved\n");
-	inputInt = scanner.nextInt();
+	String message = "";
+	while (inputInt == -1){
+	    clear();
+	    System.out.println("\nHeLlO aNd WeLcOmE tO tHe HeLp DeSk!\n\n");
+	    System.out.println( message );
+	    System.out.println("Please choose an option:\n");
+	    System.out.print("1:\tI have a Ticket ID\n");
+	    System.out.print("2:\tI have a problem\n");
+	    System.out.print("3:\tProblem solved\n");
+	    
+	    inputStr =  scanner.nextLine();
+	    try {
+		inputInt = Integer.parseInt( inputStr );
+	    }
+	    catch (Exception e){
+		message = "Selection not understood; please enter a number.";
+		inputInt = -1;
+	    }
+	    if( inputInt != -1 && inputInt != 1 
+		&& inputInt != 2 && inputInt != 3){
+		message = "Please select 1, 2, or 3.";
+		inputInt = -1;}
+		
+	}
 
 	if (inputInt == 1) {
-	    System.out.println("Please enter your Ticket ID number: ");
-	    inputID = scanner.nextInt();
+	    while( inputID == -1){
+		System.out.println("Please enter your Ticket ID number: ");
+		inputStr =  scanner.nextLine();
+		try {
+		    inputID = Integer.parseInt( inputStr );
+		}
+		catch (Exception e){
+		    inputID = -1;
+		}
+	    }
 	}
 	else if (inputInt == 2) {
 	    //Store name
